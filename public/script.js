c54601016191d67b21c88d6ac84b18c118f72db4
zulala <54187814+zulala@users.noreply.github.com>
2020-04-19 21:07:45 +0300

    nn model

--- public/script.js
+++ public/script.js
@@ -1,7 +1,9 @@
 var mymodel;
-const max_sequence_len = 22
-const lookup = 'goleyutbfackidwphrqmznvsj360x125948-7'
+var myrmodel;
+const max_sequence_len = 10
+const lookup = 'esfutrmqaodhcilbnpgvx()'
 const rules = []
+const candidate_count = 5
 function getTensor(indices){
     let padded = []
     for(var i=0;i<max_sequence_len-indices.length;i++){
@@ -10,27 +12,106 @@ function getTensor(indices){
     return tf.tensor([padded.concat(indices)])
 }
 
-function predict(str, len){
-    let indices = str.split('').map(char=> lookup.indexOf(char))
-    for(var j=0;j<len;j++){
-        ts = getTensor(indices)
-        let result = mymodel.predict(ts).dataSync()
-        let max = result[0], maxId=0
-        for(var i=1;i<result.length;i++){
-            if(result[i] > max){ 
-                max = result[i]
-                maxId = i
-            }
-        }
-        indices.push(maxId)
+function sortWithIndeces(toSort) {
+    let result = []
+    for (var i = 0; i < toSort.length; i++) {
+        result[i] = [toSort[i], i];
     }
-    return indices.map(idx=>lookup.charAt(idx)).join('')
+    result.sort(function(left, right) {
+      return left[0] > right[0] ? -1 : 1;
+    });
+    result.sortIndices = [];
+    for (var j = 0; j < toSort.length; j++) {
+        result.sortIndices.push(result[j][1]);
+        result[j] = result[j][0];
+    }
+    return result;
+}
+
+function predictNextChar(indices) {
+    ts = getTensor(indices);
+    let result = mymodel.predict(ts).dataSync();
+    let max = result[0], maxId = 0;
+    for (var i = 1; i < result.length; i++) {
+        if (result[i] > max) {
+            max = result[i];
+            maxId = i;
+        }
+    }
+    return maxId;
+}
+
+function predictPrevChar(indices) {
+    ts = getTensor(indices.reverse());
+    let result = myrmodel.predict(ts).dataSync();
+    let max = result[0], maxId = 0;
+    for (var i = 1; i < result.length; i++) {
+        if (result[i] > max) {
+            max = result[i];
+            maxId = i;
+        }
+    }
+    return maxId;
+}
+
+function predictNextChars(indices, count) {
+    ts = getTensor(indices);
+    let result = mymodel.predict(ts).dataSync();
+    return sortWithIndeces(result).sortIndices.slice(0,count);
+}
+
+
+function predictPrevChars(indices, count) {
+    ts = getTensor(indices.reverse());
+    let result = myrmodel.predict(ts).dataSync();
+    return sortWithIndeces(result).sortIndices.slice(0,count);
+}
+
+function predictNext(str, len){
+    let result = []
+    const indices = str.split('').map(char=> lookup.indexOf(char))
+    let candidateChars = predictNextChars(indices.slice(), candidate_count)
+    for(var i=0;i<candidate_count;i++){
+        let candidate = indices.slice()
+        candidate.push(candidateChars[i])
+        for(var j=1;j<len;j++){
+            let maxId = predictNextChar(candidate.slice());
+            candidate.push(maxId)
+        }
+        result.push(candidate)
+    }
+    
+    return result.map(idxs => idxs.map(idx=>lookup.charAt(idx)).join(''))
 
 }
+
+function predictPrev(str, len){
+    let result = []
+    const indices = str.split('').map(char=> lookup.indexOf(char))
+    console.log(indices.join(','))
+    let candidateChars = predictPrevChars(indices.slice(), candidate_count)
+    for(var i=0;i<candidate_count;i++){
+        let candidate = indices.slice()
+        console.log(candidate.join(','))
+        candidate.unshift(candidateChars[i])
+        for(var j=1;j<len;j++){
+            let maxId = predictPrevChar(candidate.slice());
+            candidate.unshift(maxId)
+        }
+        console.log(candidate.join(','))
+        result.push(candidate)
+    }
+    
+    return result.map(idxs => idxs.map(idx=>lookup.charAt(idx)).join(''))
+
+}
+
 async function run() {
     // Create a simple model.
-    const model = await tf.loadLayersModel('/model.json');
+    const model = await tf.loadLayersModel('/out_p/model.json');
     mymodel = model
+    const r_model = await tf.loadLayersModel('/out_r/model.json');
+    myrmodel = r_model
   }
   
 
@@ -39,9 +120,15 @@ function generate(length) {
     if($('#startRule').val()){
         result = $('#startRule').val()
     }
-    console.log(result)
-    result = predict(result, length-3)
-    console.log(result)
+
+    if($('#endRule').val()){
+        result = $('#endRule').val()
+        return predictPrev(result, length-3)
+
+    }
+
+    result = predictNext(result, length-3)
+
     return result;
 } 
 
@@ -55,37 +142,24 @@ function makeup(length) {
     return result;
 } 
 
+function createRow(name){
+    return `<tr>
+            <td>${name}</td>
+            <td>
+                <button onclick='getDomain(name, this)'>check domain</button>
+            </td>
+          </tr>`
+}
 $(document).ready(function(){
     run().then(function(){
         //console.log(predict('pexa', 3))
     });
 
     $("#generateBtn").click(function(){
-        $(".maincontainer").show()
-        var x = 0;
-        var intervalID = setInterval(function () {
+            $(".maincontainer").show()
 
-            $("#generated").html(makeup(6))
-
-        if (++x === 50) {
-            $("#generated").html('')
-            window.clearInterval(intervalID);
-            $("#generated").html(generate(6))
-        }
-        }, 30);
-    })
-    $("#startWith").click(function(event){
-        event.preventDefault()
-        event.stopPropagation()
-        let rule = document.createElement('div')
-        rule.attributes['id'] = "rule"+rules.length
-        let ruleText = document.createElement('span')
-        ruleText.innerText = "start with"
-        let ruleInput = document.createElement('input')
-        rule.appendChild(ruleText)
-        rule.appendChild(ruleInput)
-        $(".rules").append(rule)
-        return false
+            $("#nhistory>tbody").html('')
+            generate(6).map(generated => $("#nhistory>tbody").append(createRow(generated)))
     })
 
     $(".dropdown").mouseover(function(){
